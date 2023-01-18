3477cb583f893fc5910d0d1a079d101b3f0654ff
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-01-17 21:26:36 -0600

    Add basic About component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import About from '../components/About'
 
 export default function Home() {
   return (
     <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Header/>
       <Hero/>
+      <About/>
     </main>
   )
 }
---
+++ components/About.tsx
@@ -0,0 +1,12 @@
+
+const About = () => {
+  return (
+    <section id='about' className="snap-center">
+      <div className="flex flex-col relative h-screen text-center md:text-left md:flexx-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+        <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">About</h3>
+      </div>
+    </section>
+  )
+}
+
+export default About
