4758bd2eb1ce3914122f64c83dbd72658ff1c81e
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-01-23 20:51:50 -0600

    Complete Skills section

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -17,12 +17,12 @@ const Skill = ({directionLeft}: Props) => {
         }}
         transition={{duration: 1}}
         whileInView={{opacity:1, x:0}}
-        className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+        className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
         src="https://static.vecteezy.com/system/resources/previews/000/176/200/original/vector-abstract-company-logo-template-design-illustration.jpg"
       />
-      <div>
-        <div>
-          <p>100%</p>
+      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+        <div className='flex items-center justify-center h-full'>
+          <p className='text-3xl font-bold text-black opacity-100'>100%</p>
         </div>
       </div>
     </div>
