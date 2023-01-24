0d5c00decaec9acbd0cafc5d94897a0600ab334c
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-01-23 20:56:57 -0600

    Add basic projects component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 
 export default function Home() {
   return (
@@ -17,8 +18,11 @@ export default function Home() {
       <section id='experience' className='snap-center'>
         <WorkExperience/>
       </section>
-      <section id='skills' className='snap-center'>
+      <section id='skills' className='snap-start'>
         <Skills/>
+      </section>
+      <section id='projects' className='snap-start'>
+        <Projects/>
       </section>
     </main>
   )
