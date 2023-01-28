7acf180783b86b5dfd408c69ec0c37600e566f5b
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-01-27 21:58:14 -0600

    Complete projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,12 +7,20 @@ import {motion} from 'framer-motion'
 const Projects = () => {
   const projects = [1, 2, 3]
   return (
-    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
+    <motion.div 
+      initial={{opacity:0}}
+      whileInView={{opacity:1}}
+      transition={{duration: 1.5}}
+      className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
       <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
         {projects.map((project) => (
           <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen' key={project}>
-            <img
+            <motion.img
+              initial={{y:-300, opacity:0}}
+              whileInView={{opacity:1, y:0}}
+              transition={{duration: 1.2}}
+              viewport={{once:true}}
               src="https://static.vecteezy.com/system/resources/previews/000/176/200/original/vector-abstract-company-logo-template-design-illustration.jpg"
               width={400}
               height={400}
@@ -23,12 +31,15 @@ const Projects = () => {
                   Project Title
                 </span>
               </h4>
+              <p className='text-lg text-center md:text-left'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
             </div>
           </div>
         ))}
       </div>
       <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'/>
-    </div>
+    </motion.div>
   )
 }
 
