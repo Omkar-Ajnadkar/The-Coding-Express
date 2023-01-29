fe436c932ad7fbd435a7672c40cfe3a1274ffda5
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-01-28 20:21:08 -0600

    Add customized scrollbars

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import ContactMe from '../components/ContactMe'
 
 export default function Home() {
   return (
-    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Header/>
       <section id="hero" className="snap-start">
         <Hero/>
