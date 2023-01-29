9ed94d99e6624cf8df487cb46c06110bc2148da5
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-01-28 23:06:56 -0600

    Initiate Sanity project

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'blackbird71sr',
+
+  projectId: '52bkdx7j',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
