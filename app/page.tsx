db59c475f0acf51506ae11a63923f118e92acf72
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-02-01 19:40:38 -0600

    Remove console log

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,6 @@ const fetcher = async () => {
 
 export default function Home () {
   const { data } = useSWR('data', fetcher)
-  console.log(data)
 
   return (
     <main className="z-0 h-screen snap-y snap-mandatory overflow-y-scroll bg-[rgb(36,36,36)] text-white overflow-x-hidden scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
